1832e6c41fb0e2b6db2bef3857d430d291217ab0
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-01-05 16:49:34 +0200

    Add styles for collapsing the dashboard on large screens

--- admin/admin.js
+++ admin/admin.js
@@ -5,3 +5,13 @@ const sidebarToggle = () => {
 const sidebarTrigger = document.getElementById("main-header-sidebar-toggle");
 
 sidebarTrigger.addEventListener("click", sidebarToggle);
+
+// Sidebar collapse
+
+const sidebarCollapse = () => {
+	document.body.classList.toggle("sidebar-collapsed");
+};
+
+const sidebarCollapseTrigger = document.getElementById("sidebar-collapse");
+
+sidebarCollapseTrigger.addEventListener("click", sidebarCollapse);
